57bd10057afcda772464f2f293153258b58d4bed
kentaras <kentaraz@gmail.com>
2018-12-24 03:10:50 +0200

    Logout fixed

--- client/src/body/admin/components/AddNewUser.js
+++ client/src/body/admin/components/AddNewUser.js
@@ -12,13 +12,13 @@ class AddNewUser extends Component {
     }
 
     addUser(e) {
-        e.preventDefault()
-        let token = localStorage.getItem('token')
+        e.preventDefault();
+        let token = localStorage.getItem('token');
         userActions.addUser(this.state.name, this.state.password, this.state.admin, token)
             .then(res => res.json())
             .then(data => {
                 if(data.success === true) {
-                    window.alert('User Added')
+                    window.alert('User Added');
                     this.props.added()
                 } else {
                     console.log(data)
@@ -34,12 +34,12 @@ class AddNewUser extends Component {
                 <form>
                     <label>Vartotojo vardas</label><br/>
                     <input type='text' placeholder='Jūsų vardas' value={ name } 
-                        onChange={(e) => this.setState({name: e.target.value})}></input><br/>
+                        onChange={(e) => this.setState({name: e.target.value})}/><br/>
                     <label>Slaptažodis</label><br/>
                     <input type='password' placeholder='Slaptažodis' value={ password } 
-                        onChange={(e) => this.setState({password: e.target.value})} autoComplete='password'></input><br/>
+                        onChange={(e) => this.setState({password: e.target.value})} autoComplete='password'/><br/>
                     <label>Administratorius</label>
-                    <input type='checkbox' unchecked='true' onChange={() => this.setState({admin: !admin})}></input><br/>
+                    <input type='checkbox' onChange={() => this.setState({admin: !admin})}/><br/>
                     <button onClick={(e) => this.addUser(e)} className='btn submitBtn'> Pridėti </button>
                 </form>
             </div>
